721a66f0a0fe9e8474ca9ac4862585eb0be67cee
yghari <ghariyouness90@gmail.com>
2023-07-25 15:33:41 +0100

    first task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,44 +1,37 @@
 #include "sort.h"
 /**
- * swaping - Swaps two integer values.
- *
- * @first: Pointer to the first value to be swapped.
- * @second: Pointer to the second value to be swapped.
+ * swap - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
  */
-void swaping(int *first, int *second)
+void swap(int *a, int *b)
 {
-	int tmp = *first;
-	*first = *second;
-	*second = tmp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
-
 /**
- * bubble_sort - Sorts an array of integers using the bubble sort algorithm.
- *
- * @array: Pointer to the array to be sorted.
- * @size: Size of the array to be sorted.
+ * bubble_sort - sorts an array of integers.
+ * @array: array of integers to sort.
+ * @size: size of the array.
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	bool swapped;
 
 	if (size < 2 || array == NULL)
 		return;
-
 	for (i = size - 1; i >= 1; i--)
 	{
-		swapped = false;
 		for (j = 1; j <= i; j++)
 		{
 			if (array[j - 1] > array[j])
 			{
-				swaping(&array[j - 1], &array[j]);
+				swap(&array[j - 1], &array[j]);
 				print_array(array, size);
-				swapped = true;
 			}
 		}
-		if (!swapped)
-			break;
 	}
 }
+
